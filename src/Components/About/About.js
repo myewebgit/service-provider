d51c624751dd7ec27aefbeb3dc6c679f9d986e10
myewebgit <mnislamctg7@gmail.com>
2022-04-18 22:44:36 +0600

    Database info updated

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -3,26 +3,28 @@ import React from "react";
 const About = () => {
     return(
         <div>
-            <h4> This is About my projects</h4>
-            <h5>Web developers need a combination of graphic design skills and technical computer skills that will allow them to create particular designs on web pages. In addition to looking nice, websites need to be functional and secure. It is the responsibility of web developers to create such sites that meet the requirements of employers or clients.
+            <h4> This is about me and my future plan to be a web developer.....</h4>
+            <h5> I am Md. Nurul Islam, a trainee of complete web development course with Jhankar Mahbub launched by Programming Hero. As a non-tech background candidate of this course, I need to pay the height attention to this course. First I need to explane the Responsibilities and skilles required for a web developer. <br></br> Web developers need a combination of graphic design skills and technical computer skills that will allow them to create particular designs on web pages. In addition to looking nice, websites need to be functional and secure. It is the responsibility of web developers to create such sites that meet the requirements of employers or clients.<br></br>
 
 
 Web developers often implement ideas from less tech-savvy colleagues who lack the know-how to turn their vision into an actual, functional website.
 
 
 Web Developer Duties & Responsibilities
-This job generally requires the ability to do the following work:1﻿
+This job generally requires the ability to do the following work:<br></br><br></br><br></br>
 
+1.Know HTML, CSS, JavaScript, PHP, and other relevant web design coding languages<br></br>
+2.Create and test applications for websites<br></br>
+3.Collaborate<br></br>
+4.Present design specs<br></br>
+5.Work with graphics and other designers<br></br>
+6.Troubleshoot website problems<br></br>
+7.Maintain and update websites<br></br>
+8.Monitor website traffic<br></br>
+9.Stay up-to-date on technology<br></br><br></br><br></br>
 
-Know HTML, CSS, JavaScript, PHP, and other relevant web design coding languages
-Create and test applications for websites
-Collaborate
-Present design specs
-Work with graphics and other designers
-Troubleshoot website problems
-Maintain and update websites
-Monitor website traffic
-Stay up-to-date on technology</h5>
+To make me skilled in this sector I need to do hard work and need to keep me on track of this course, I hope I will do my best in this course and will get me a skilled professional in this arena.
+</h5>
         </div>
     );
 };
