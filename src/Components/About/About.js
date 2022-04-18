bcf9c7ef68510690e0e8f61ba3e493e1cdc0e115
myewebgit <mnislamctg7@gmail.com>
2022-04-18 22:04:40 +0600

    Description of my future plan in about section

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -4,6 +4,25 @@ const About = () => {
     return(
         <div>
             <h4> This is About my projects</h4>
+            <h5>Web developers need a combination of graphic design skills and technical computer skills that will allow them to create particular designs on web pages. In addition to looking nice, websites need to be functional and secure. It is the responsibility of web developers to create such sites that meet the requirements of employers or clients.
+
+
+Web developers often implement ideas from less tech-savvy colleagues who lack the know-how to turn their vision into an actual, functional website.
+
+
+Web Developer Duties & Responsibilities
+This job generally requires the ability to do the following work:1﻿
+
+
+Know HTML, CSS, JavaScript, PHP, and other relevant web design coding languages
+Create and test applications for websites
+Collaborate
+Present design specs
+Work with graphics and other designers
+Troubleshoot website problems
+Maintain and update websites
+Monitor website traffic
+Stay up-to-date on technology</h5>
         </div>
     );
 };
