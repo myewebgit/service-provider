528bace9873ecf824c17ff82f35924947a14c537
myewebgit <mnislamctg7@gmail.com>
2022-04-18 22:58:04 +0600

    Update Banner pic and details

--- src/Components/Home/Banner/Banner.js
+++ src/Components/Home/Banner/Banner.js
@@ -14,37 +14,39 @@ const Banner = () => {
       <Carousel.Item>
         <img
           className="d-block w-100"
-          src="https://i.ibb.co/xFR7nHg/5efc4d6b35ae9-manish-blocg-banner.png"
+          src="https://i.ibb.co/Jj56Z1B/ban1.jpg
+          "
           alt="First slide"
         />
         <Carousel.Caption>
-          <h3>First slide label</h3>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          <h3>Accounting</h3>
+          <p> We provide service in Tax, Audit and Bookkeeping.</p>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
         <img
           className="d-block w-100"
-          src="https://i.ibb.co/xFR7nHg/5efc4d6b35ae9-manish-blocg-banner.png"
+          src="https://i.ibb.co/1vKXH1T/ban2.jpg
+          "
           alt="Second slide"
         />
 
         <Carousel.Caption>
-          <h3>Second slide label</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          <h3>Bookkeeping</h3>
+          <p> We provide service in Tax, Audit and Bookkeeping.</p>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
         <img
           className="d-block w-100"
-          src="https://i.ibb.co/xFR7nHg/5efc4d6b35ae9-manish-blocg-banner.png"
+          src="https://i.ibb.co/fqWGGpq/ban3.jpg"
           alt="Third slide"
         />
 
         <Carousel.Caption>
-          <h3>Third slide label</h3>
+          <h3>Taxation and Auditing</h3>
           <p>
-            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+            We provide service in Tax, Audit and Bookkeeping. 
           </p>
         </Carousel.Caption>
       </Carousel.Item>
